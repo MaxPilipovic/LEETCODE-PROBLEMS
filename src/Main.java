c826292eba0972e6c53ffc6a5c19a0e6d9399408
MaxPilipovic <maxpkusa@gmail.com>
2024-05-11 11:49:44 -0700

    DUPLICATE ENTRY EASY PROBLEM

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Arrays;
+//Duplicate Elements EASY
+
 public class Main {
     public static void main (String[] args) {
-        //aids
-        //aids2
+        int[] array = {1, 2, 2, 3, 3, 4, 5};
+        int index = 1;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] != array[i + 1]) {
+                array[index] = array[i + 1];
+                index++;
+            }
+        }
+        System.out.println(index);
+        System.out.println(Arrays.toString(array));
     }
 }
